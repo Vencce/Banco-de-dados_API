93fd95256e0ab00a08eefa2b49bc6c124415a4d0
Vitor-Ferreira <101403160+Vencce@users.noreply.github.com>
2022-10-26 15:07:58 -0300

    Update bd-2info.sql

--- bd-2info.sql
+++ bd-2info.sql
@@ -25,7 +25,7 @@ cad_usuario.tipuser_cd = tipo_usuario.cod_tip_user;
 
 select * from pedidos;
 
-select nome, cpf, cod_pedido, dtped
+select nome, cpf, cod_pedido, dtped2) CRUC que retorne: quantidade de pedidos agrupando pela sigla da UF
 from cad_usuario, pedidos
 where cad_usuario.cpf = pedidos.cad_usuario_cpf;
 
@@ -113,24 +113,93 @@ from cad_usuario, tipo_usuario
 where cad_usuario.tipuser_cd = tipo_usuario.cod_tip_user 
 group by descricao_tipo order by count(tipuser_cd) desc;
 
+/*----------------------------------------------------------19/10--------------------------------------------------------------------------*/
+
 /*1) CRUC que retorne: nome dos usuários e a quantidade de pedidos realizados por cada usuário.*/
 
-select nome, count(dtped)
+select nome, count(cad_usuario_cpf)
 from cad_usuario, pedidos
 where cad_usuario.cpf = pedidos.cad_usuario_cpf
 group by nome;
 
 /*2) CRUC que retorne: nome do usuário e o valor total do pedido, filtrando por um código de pedido válido.*/
 
-
+select nome, sum(preco_unit * qtditem)
+from cad_usuario,pedidos,itemped,produto
+where cad_usuario.cpf = pedidos.cad_usuario_cpf and
+pedidos.cod_pedido = itemped.ped_codpedidos and
+itemped.prod_cod_produto = produto.cod_produto and
+pedidos.cod_pedido = 1 group by nome;
 
 /*3) CRUC que retorne: a quantidade de logradouros filtrando pela sigla do estado.*/
 
+select ds_uf_sigla, count(cd_logradouro)
+from logradouro, bairros, cidades, uf
+where logradouro.bairros_cd_bairro = bairros.cd_bairro and
+bairros.cidade_cd_cidade = cidades.cd_cidade and
+cidades.uf_cd_uf = uf.cd_uf group by ds_uf_sigla;
 
 
 /*4) CRUC que retorne: a quantidade de bairros agrupando pela sigla do estado.*/
 
-
+select ds_uf_sigla, count(cd_bairro)
+from bairros, cidades, uf
+where bairros.cidade_cd_cidade = cidades.cd_cidade and
+cidades.uf_cd_uf = uf.cd_uf group by ds_uf_sigla;
 
 /*5) CRUC que retorne: preço total dos pedidos agrupando pelo código dos pedidos.*/
+
+select cod_pedido, sum(preco_unit * qtditem)
+from pedidos, itemped, produto
+where pedidos.cod_pedido = itemped.ped_codpedidos and
+itemped.prod_cod_produto = prod_cod_produto group by cod_pedido;
+
+/*--------------------------------------------------------------26/10----------------------------------------------------------------------*/
+
+
+/*1) CRUC que retorne: descrição do produto e a quantidade de vezes que ele foi comprado, agrupando pela descrição do produto.*/
+
+/*2) CRUC que retorne: quantidade de pedidos agrupando pela sigla UF.*/
+
+/*3) CRUC que retorne: quantidade de usuarios agrupando pelo estado.*/
+/*1) CRUC que retorne: nome dos usuários e a quantidade de pedidos realizados por cada usuário.*/
+117
+​
+118
+select nome, count(dtped)
+119
+from cad_usuario, pedidos
+120
+where cad_usuario.cpf = pedidos.cad_usuario_cpf
+121
+group by nome;
+122
+​
+123
+/*2) CRUC que retorne: nome do usuário e o valor total do pedido, filtrando por um código de pedido válido.*/
+124
+​
+125
+​
+126
+​
+127
+/*3) CRUC que retorne: a quantidade de logradouros filtrando pela sigla do estado.*/
+128
+​
+129
+​
+130
+​
+131
+/*4) CRUC que retorne: a quantidade de bairros agrupando pela sigla do estado.*/
+132
+​
+133
+​
+134
+​
+135
+/*5) CRUC que retorne: preço total dos pedidos agrupando pelo código dos pedidos.*/
+136
 
